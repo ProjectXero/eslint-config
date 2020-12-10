47c2a428c3f3e8b9a97c48f6c8f53150b06d0c2e
Matthew Lanigan <rintaun@gmail.com>
2020-12-09 20:33:38 -0500

    chore: add semantic-release config

---
+++ release.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        tarballDir: 'release',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'release/*.tgz',
+      },
+    ],
+    '@semantic-release/git',
+  ],
+}
